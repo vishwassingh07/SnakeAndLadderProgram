e06bb68a8b6bcb1290dd9949def64514ef521120
Vishwas <baghel.vishwas0@gmail.com>
2022-06-06 14:34:52 +0530

    [Vishwas]add: Single Player Game Starting With Zero Position

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace SnakeAndLadderProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Snake And Ladder Program");
+            SnakeAndLadderGame game1 = new SnakeAndLadderGame();
+            game1.SnakeandLadder();
+
+        }
+    }
+}
---
+++ SnakeAndLadderProblem/SnakeAndLadderGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class SnakeAndLadderGame
+    {
+        public void SnakeandLadder()
+        {
+            int position = 0;
+            Console.WriteLine("Player Starting Position : {0}", position);
+        }
+    }
+}
